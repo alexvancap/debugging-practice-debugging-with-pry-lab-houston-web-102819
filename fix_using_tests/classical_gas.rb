2aade25f6e5cb360047e184de864184513cd7468
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-17 21:05:52 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -1,4 +1,7 @@
 require 'pry'
+petrol_petes = GasStation.new("Petrol Pete's", 50)
+seashore_shell = GasStation.new("The Seashore Shell", 40)
+dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
 class GasStation
 
   attr_reader :brand, :unleaded_price
@@ -7,13 +10,7 @@ class GasStation
     @brand = brand
     @unleaded_price = unleaded_price
   end
-  petrol_petes = GasStation.new("Petrol Pete's", 50)
-  seashore_shell = GasStation.new("The Seashore Shell", 40)
-  dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
 
   binding.pry
 
 end
-
-
-
