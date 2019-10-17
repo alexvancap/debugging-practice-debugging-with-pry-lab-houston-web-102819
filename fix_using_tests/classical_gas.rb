4d422541c5f66fdb62c8b615006a582e4357cf49
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-17 21:04:37 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -8,11 +8,12 @@ class GasStation
     @unleaded_price = unleaded_price
   end
 
+
+  binding.pry
+
 end
 
 
 petrol_petes = GasStation.new("Petrol Pete's", 50)
 seashore_shell = GasStation.new("The Seashore Shell", 40)
 dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
-
-binding.pry
