a0ce03482c47969d47073182efb9e84a0f7bf063
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-17 21:02:38 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -6,7 +6,7 @@ class GasStation
   def initialize(brand, unleaded_price)
     @brand = brand
     @unleaded_price = unleaded_price
-    dinobones.unleaded_price
+    @dinobones.unleaded_price
   end
 
 end
