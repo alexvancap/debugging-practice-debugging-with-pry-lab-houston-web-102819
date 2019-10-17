362b89e2ba275f53c4d8b1a502161d53c357bd94
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-17 21:06:24 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -12,6 +12,7 @@ class GasStation
 
 end
 
-GasStation.new("Petrol Pete's", 50)
+petrol_petes = GasStation.new("Petrol Pete's", 50)
 seashore_shell = GasStation.new("The Seashore Shell", 40)
 dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
+
