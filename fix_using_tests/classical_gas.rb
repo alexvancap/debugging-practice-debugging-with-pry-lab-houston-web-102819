e0e3b80a3b39a5d757b985bed4d12edc087f9e36
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-17 21:06:34 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -4,8 +4,8 @@ class GasStation
   attr_reader :brand, :unleaded_price
 
   def initialize(brand, unleaded_price)
-    @brand = brand
-    @unleaded_price = unleaded_price
+    @brand = &brand
+    @unleaded_price = &unleaded_price
   end
 
   binding.pry
@@ -15,4 +15,3 @@ end
 petrol_petes = GasStation.new("Petrol Pete's", 50)
 seashore_shell = GasStation.new("The Seashore Shell", 40)
 dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
-
